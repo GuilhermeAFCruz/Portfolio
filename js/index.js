8b5c3d2527928c47d1ee018932336af074bb96ae
Guilherme Augusto <guilhermelive980@hotmail.com>
2022-12-06 13:09:01 -0300

    algumas mudanças

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const navToggle = document.querySelector('.nav-toggle')
 const navLinks = document.querySelectorAll('.nav__link')
+const navBtn = document.querySelector('[data-nav-button]')
 
+
+// Nav toggle menu
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open')
 })
@@ -8,5 +11,22 @@ navToggle.addEventListener('click', () => {
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
+        
     })
 })
+
+// Nav button animation toggle
+
+navBtn.addEventListener('click', () => {
+    const isOpened = navBtn.getAttribute('aria-expanded') === 'true'
+    
+    if(isOpened){
+        navBtn.setAttribute('aria-expanded', 'false')
+    } else {
+        navBtn.setAttribute('aria-expanded','true')
+    }
+
+}
+)
+
+
